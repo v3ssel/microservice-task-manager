f4254f957a0df95f4b9762a2ac82467c0bdd3ae8
v3ssel <v3ssels@pm.me>
2023-11-22 01:05:42 +0600

    swagger added

--- authorization/Program.cs
+++ authorization/Program.cs
@@ -8,23 +8,33 @@ internal class Program
     private static void Main(string[] args)
     {
         var builder = WebApplication.CreateBuilder(args);
+
         var authOpt = builder.Configuration.GetSection("AuthOptions").Get<AuthOptions>()
                         ?? throw new ArgumentException("AuthOptions is required.");
-
         builder.Services.AddSingleton(authOpt);
-        builder.Services.AddTransient<TokenService>();
-        builder.Services.AddControllers();
+        builder.Services.AddAsymmetricAuthetication(authOpt);
 
         var conn_str = builder.Configuration.GetConnectionString("MongoDB") ?? throw new ArgumentException("The MongoDB connection string is not set.");
         builder.Services.AddSingleton(new MongoClient(conn_str));
+        builder.Services.AddTransient<TokenService>();
+        
+        builder.Services.AddControllers();
 
-        builder.Services.AddAsymmetricAuthetication(authOpt);
+        builder.Services.AddEndpointsApiExplorer();
+        builder.Services.AddSwaggerGen();
 
         var app = builder.Build();
-        app.UseAuthentication();
-        app.UseAuthorization();
+
+        if (app.Environment.IsDevelopment())
+        {
+            app.UseSwagger();
+            app.UseSwaggerUI();
+        }
 
         app.UseHttpsRedirection();
+
+        app.UseAuthentication();
+        app.UseAuthorization();
 
         app.MapControllers();
 
